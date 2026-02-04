5503f4efae12542bce24953ffe503464ce84e429
igorkll <frerther@gmail.com>
2026-02-05 01:16:56 +0300

    backup

--- uflinux/chroot/setup.sh
+++ uflinux/chroot/setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+# disable getty
 systemctl mask getty.target
 
+# create user
+
+loginctl enable-linger user
+
+# install DE
+apt install --no-install-recommends \
+  plasma-workspace \
+  plasma-workspace-wayland \
+  kwin-wayland \
+  plasma-session
+
+# setup DE
+systemctl enable shell.service
