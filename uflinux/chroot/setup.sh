1e65bd1d8957fc64c23d219911e20bef53a9c65b
igorkll <frerther@gmail.com>
2026-02-05 07:03:12 +0300

    Update setup.sh

--- uflinux/chroot/setup.sh
+++ uflinux/chroot/setup.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 set -e
 
-# ------------ set root password (for debug)
-echo "root:root" | chpasswd
-usermod -s /bin/bash root
+DEBUG=true
 
-# ------------ disable getty
-# systemctl mask getty.target
+if [ "$DEBUG" = "true" ]; then
+    # ------------ set root password (for debug)
+    echo "root:root" | chpasswd
+    usermod -s /bin/bash root
+else
+    # ------------ disable getty
+    systemctl mask getty.target
+end
 
 # ------------ create user
 useradd -m -s /bin/bash user
 usermod -aG video,input,audio,render user
-# passwd -d user
-echo "user:user" | chpasswd
+if [ "$DEBUG" = "true" ]; then
+    echo "user:user" | chpasswd
+else
+    passwd -d user
+fi
 
 # ------------ setup DE
-systemctl enable sddm
-echo "sddm shared/default-display-manager select sddm" | debconf-set-selections
-systemctl set-default graphical.target
+if [ "$DEBUG" != "true" ]; then
+    systemctl enable sddm
+    echo "sddm shared/default-display-manager select sddm" | debconf-set-selections
+    systemctl set-default graphical.target
+fi
 
 # ------------ remove trash
