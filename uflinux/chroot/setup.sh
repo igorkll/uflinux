42df65b3fba1e2b5f84a5a6320fb3983a7dcbec6
igorkll <frerther@gmail.com>
2026-02-05 07:08:22 +0300

    Update setup.sh

--- uflinux/chroot/setup.sh
+++ uflinux/chroot/setup.sh
@@ -29,3 +29,19 @@ if [ "$DEBUG" != "true" ]; then
 fi
 
 # ------------ remove trash
+
+# remove documentation
+rm -rf /usr/share/man
+rm -rf /usr/share/doc
+rm -rf /usr/share/info
+
+# remove package manager
+rm -f /usr/bin/apt*
+rm -f /usr/bin/dpkg*
+rm -rf /usr/lib/apt
+rm -rf /usr/lib/dpkg
+rm -rf /usr/share/dpkg
+rm -rf /var/lib/apt
+rm -rf /var/lib/dpkg
+rm -rf /etc/apt
+rm -rf /etc/dpkg
