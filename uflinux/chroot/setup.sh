270b5818b4ef77a6bf3b88121445200c08415c5a
igorkll <frerther@gmail.com>
2026-02-06 20:36:28 +0300

    Update setup.sh

--- uflinux/chroot/setup.sh
+++ uflinux/chroot/setup.sh
@@ -73,16 +73,18 @@ rm -rf /usr/share/man
 rm -rf /usr/share/doc
 rm -rf /usr/share/info
 
-# remove package manager
-rm -f /usr/bin/apt*
-rm -f /usr/bin/dpkg*
-rm -rf /usr/lib/apt
-rm -rf /usr/lib/dpkg
-rm -rf /usr/share/dpkg
-rm -rf /var/lib/apt
-rm -rf /var/lib/dpkg
-rm -rf /etc/apt
-rm -rf /etc/dpkg
+if [ "$DEBUG" != "true" ]; then
+    # remove package manager
+    rm -f /usr/bin/apt*
+    rm -f /usr/bin/dpkg*
+    rm -rf /usr/lib/apt
+    rm -rf /usr/lib/dpkg
+    rm -rf /usr/share/dpkg
+    rm -rf /var/lib/apt
+    rm -rf /var/lib/dpkg
+    rm -rf /etc/apt
+    rm -rf /etc/dpkg
+fi
 
 # remove user system
 rm -f /usr/sbin/adduser
