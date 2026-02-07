6b9a9335b7b6ec9d546205c6197c72ca1dd31ea9
igorkll <frerther@gmail.com>
2026-02-07 11:31:10 +0300

    backup

--- uflinux/hooks/customize-1.sh
+++ uflinux/hooks/customize-1.sh
@@ -12,14 +12,24 @@ install_dir() {
     local src="$1"
     local dst="$2"
 
+    local tmp
+    tmp=$(mktemp -d)
+
+    cp -r "$src"/. "$tmp"/
+    chown -R 0:0 "$tmp"
+    chmod -R 1755 "$tmp"
+    chmod --reference="$src" "$tmp"
+    chown --reference="$src" "$tmp"
+
     mkdir -p "$dst"
-    cp -r "$src"/. "$dst"/
-    chown -R 0:0 "$dst"
-    chmod -R 1755 "$dst"
+    cp "$tmp" "$dst"
+
+    rm -rf "$tmp"
 }
 
 # ----------------- copy files
 install_dir "files/bootlogo" "$1/usr/share/plymouth/themes/bootlogo"
+install_dir "files/embedded-plymouth/amd64" "$1"
 
 # ----------------- chroot scripts
 run_in_chroot "$1" hooks/chroot/boot_logo.sh
