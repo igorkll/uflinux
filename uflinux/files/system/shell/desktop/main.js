38bb7aeb17992227e7cf20d2c386f84230f53638
igorkll <frerther@gmail.com>
2026-02-08 19:47:47 +0300

    qwer

--- uflinux/files/system/shell/desktop/main.js
+++ uflinux/files/system/shell/desktop/main.js
@@ -1,10 +1,32 @@
+{
+
 function updateTime() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    document.getElementById("time").textContent = `${hours}:${minutes}:${seconds}`;
 }
 
+function updateWallpaper() {
+    let src = "wallpapers/12.jpg";
+    let isVideo = false;
+    let contentFit = 'contains';
+
+    let wallpaperImg = document.getElementById("wallpaperImg");
+    let wallpaperVideo = document.getElementById("wallpaperVideo");
+    if (isVideo) {
+        wallpaperVideo.src = src;
+        wallpaperVideo.classList.contentFit = contentFit;
+        wallpaperVideo.classList.contentFit = contentFit;
+    } else {
+        wallpaperImg.src = src;
+        wallpaperImg.classList.contentFit = contentFit;
+    }
+}
+
+updateWallpaper();
 updateTime();
 setInterval(updateTime, 1000);
+
+}
