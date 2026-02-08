724c7a538d4d3d8970538ecf1c884614d38d3d93
igorkll <frerther@gmail.com>
2026-02-08 23:53:05 +0300

    backup

--- uflinux/files/system/shell/desktop/storage.js
+++ uflinux/files/system/shell/desktop/storage.js
@@ -6,4 +6,23 @@ let defaultStorage = {
     }
 }
 
-let storage
+let storage = {}
+
+function storage_load() {
+    let storageData = localStorage.getItem("storageData");
+    if (storageData) {
+        try {
+            storage = JSON.parse(storageData);
+        } catch (e) {
+            storage = {};
+        }
+    } else {
+        storage = {};
+    }
+
+    mergeTables(storage, defaultStorage);
+}
+
+function storage_save() {
+    localStorage.setItem("storageData", JSON.stringify(storage));
+}
---
+++ uflinux/files/system/shell/desktop/utils.js
@@ -0,0 +1,9 @@
+function mergeTables(tbl, def) {
+    for (let key in def) {
+        if (!(key in tbl)) {
+            tbl[key] = def[key];
+        } else if (typeof def[key] === 'object' && def[key] != null && !Array.isArray(def[key])) {
+            mergeTables[tbl[key], def[key]]
+        }
+    }
+}
