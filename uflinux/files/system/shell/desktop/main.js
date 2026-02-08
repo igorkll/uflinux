2e5b375f2667eee073bda2e1829270ff2749dbad
igorkll <frerther@gmail.com>
2026-02-08 19:51:18 +0300

    backup

--- uflinux/files/system/shell/desktop/main.js
+++ uflinux/files/system/shell/desktop/main.js
@@ -12,16 +12,23 @@ function updateWallpaper() {
     let src = "wallpapers/12.jpg";
     let isVideo = false;
     let contentFit = 'contains';
+    let background = '#ff0000';
+
+    let wallpaperBase = document.getElementById("wallpaperBase");
+    wallpaperBase.classList.background = background;
 
     let wallpaperImg = document.getElementById("wallpaperImg");
     let wallpaperVideo = document.getElementById("wallpaperVideo");
     if (isVideo) {
         wallpaperVideo.src = src;
         wallpaperVideo.classList.contentFit = contentFit;
-        wallpaperVideo.classList.contentFit = contentFit;
+        wallpaperVideo.classList.display = 'initial';
+        wallpaperImg.classList.display = 'none';
     } else {
         wallpaperImg.src = src;
         wallpaperImg.classList.contentFit = contentFit;
+        wallpaperImg.classList.display = 'initial';
+        wallpaperVideo.classList.display = 'none';
     }
 }
 
