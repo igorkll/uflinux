5c7080259db158bab4da5a98ff2045384aa5508d
igorkll <frerther@gmail.com>
2026-02-08 19:53:45 +0300

    Update main.js

--- uflinux/files/system/shell/desktop/main.js
+++ uflinux/files/system/shell/desktop/main.js
@@ -11,24 +11,24 @@ function updateTime() {
 function updateWallpaper() {
     let src = "wallpapers/12.jpg";
     let isVideo = false;
-    let contentFit = 'contains';
+    let objectFit = 'contain';
     let background = '#ff0000';
 
     let wallpaperBase = document.getElementById("wallpaperBase");
-    wallpaperBase.classList.background = background;
+    wallpaperBase.style.background = background;
 
     let wallpaperImg = document.getElementById("wallpaperImg");
     let wallpaperVideo = document.getElementById("wallpaperVideo");
     if (isVideo) {
         wallpaperVideo.src = src;
-        wallpaperVideo.classList.contentFit = contentFit;
-        wallpaperVideo.classList.display = 'initial';
-        wallpaperImg.classList.display = 'none';
+        wallpaperVideo.style.objectFit = objectFit;
+        wallpaperVideo.style.display = 'initial';
+        wallpaperImg.style.display = 'none';
     } else {
         wallpaperImg.src = src;
-        wallpaperImg.classList.contentFit = contentFit;
-        wallpaperImg.classList.display = 'initial';
-        wallpaperVideo.classList.display = 'none';
+        wallpaperImg.style.objectFit = objectFit;
+        wallpaperImg.style.display = 'initial';
+        wallpaperVideo.style.display = 'none';
     }
 }
 
