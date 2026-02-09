c4a005a19a2b38a48b1ef6805a1b76b4f8497a16
igorkll <frerther@gmail.com>
2026-02-09 16:07:46 +0300

    Update getSystemInfo.js

--- uflinux/files/system/shell/desktop/getSystemInfo.js
+++ uflinux/files/system/shell/desktop/getSystemInfo.js
@@ -1,5 +1,26 @@
 {
 
+const applicationsDirs = [
+    "/usr/share/applications",
+    "/usr/local/share/applications",
+    path.join(process.env.HOME || "", ".local/share/applications")
+];
+
+window.getAllDesktopFiles = function() {
+    const apps = [];
+
+    applicationsDirs.forEach(dir => {
+        const files = fs.readdirSync(dir);
+        files.forEach(file => {
+            if (file.endsWith(".desktop")) {
+                apps.push(path.join(dir, file));
+            }
+        });
+    });
+
+    return apps;
+}
+
 window.getAllApps = function() {
     
 }
