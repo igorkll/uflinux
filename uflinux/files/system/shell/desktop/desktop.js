da2b20b71486ad9cdcedb1709af3fba1da471162
igorkll <frerther@gmail.com>
2026-02-09 15:38:50 +0300

    jnk,

--- uflinux/files/system/shell/desktop/desktop.js
+++ uflinux/files/system/shell/desktop/desktop.js
@@ -2,19 +2,23 @@
 
 let appsTabHost = document.getElementById("appsTabHost")
 
-function addIcon(appsTab, img) {
+function addIcon(appsTab, x, y, imgPath) {
+    let appIcon = document.createElement("div")
+    appIcon.classList.add("appIcon")
 
+    appsTab.appendChild(appIcon)
 }
 
 function addAppsTab() {
     let appsTab = document.createElement("div")
     appsTab.classList.add("appsTab")
 
+    addIcon(appsTab, 2, 2, "wallpapers/1.jpg")
+
     appsTabHost.appendChild(appsTab)
 }
 
-document.querySelectorAll(".appsTab").forEach(appsTab => {
-
-})
+addAppsTab()
+addAppsTab()
 
 }
