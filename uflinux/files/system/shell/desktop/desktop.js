a72c593a6e1bd6e62ecb4e09336b23c127792da7
igorkll <frerther@gmail.com>
2026-02-09 15:34:06 +0300

    qwe

--- uflinux/files/system/shell/desktop/desktop.js
+++ uflinux/files/system/shell/desktop/desktop.js
@@ -1,7 +1,20 @@
-function addIcon(appsTab, src) {
+{
 
+let appsTabHost = document.getElementById("appsTabHost")
+
+function addIcon(appsTab, img) {
+
+}
+
+function addAppsTab() {
+    let appsTab = document.createElement("div")
+    appsTab.classList.add("appsTab")
+
+    appsTabHost.appendChild(appsTab)
 }
 
 document.querySelectorAll(".appsTab").forEach(appsTab => {
 
 })
+
+}
