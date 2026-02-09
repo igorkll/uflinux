89c8acad3a7af34f42d11cfd806bdcb092f0fa84
igorkll <frerther@gmail.com>
2026-02-09 22:25:45 +0300

    backup

---
+++ uflinux/files/system/shell/desktop/mouseSwipe.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.appsTabHost');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => {
+    isDown = false;
+    container.classList.remove('active');
+});
+
+container.addEventListener('mouseup', () => {
+    isDown = false;
+    container.classList.remove('active');
+});
+
+container.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = x - startX;
+    container.scrollLeft = scrollLeft - walk;
+});
