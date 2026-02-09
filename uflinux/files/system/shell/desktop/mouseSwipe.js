ca68c350f250389442623e66abbaad7356f3fa4c
igorkll <frerther@gmail.com>
2026-02-09 22:38:32 +0300

    asd

--- uflinux/files/system/shell/desktop/mouseSwipe.js
+++ uflinux/files/system/shell/desktop/mouseSwipe.js
@@ -1,29 +1,43 @@
-const container = document.querySelector('.appsTabHost');
+{
 
-let isDown = false;
-let startX;
-let scrollLeft;
+const container = document.querySelector('.appsTabHost')
+
+let isDown = false
+let startX
+let scrollLeft
+let target
 
 container.addEventListener('mousedown', (e) => {
-    isDown = true;
-    startX = e.pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-});
+    isDown = true
+    startX = e.pageX - container.offsetLeft
+    scrollLeft = container.scrollLeft
+    container.classList.add("disableSnap")
+})
+
+function scrollEnd() {
+    isDown = false
+    container.classList.remove('active')
+}
 
 container.addEventListener('mouseleave', () => {
-    isDown = false;
-    container.classList.remove('active');
-});
+    scrollEnd()
+})
 
 container.addEventListener('mouseup', () => {
-    isDown = false;
-    container.classList.remove('active');
-});
+    scrollEnd()
+})
 
 container.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - container.offsetLeft;
-    const walk = x - startX;
-    container.scrollLeft = scrollLeft - walk;
-});
+    if (!isDown) return
+    e.preventDefault()
+    const x = e.pageX - container.offsetLeft
+    const walk = x - startX
+    target = scrollLeft - walk
+
+    container.scrollTo({
+        left: target,
+        behavior: 'auto'
+    })
+})
+
+}
