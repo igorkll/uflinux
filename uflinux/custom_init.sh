c24dd4baccad2452fa8a0d647d3daaed3f2df8d7
igorkll <frerther@gmail.com>
2026-02-10 17:06:57 +0300

    Update custom_init.sh

--- uflinux/custom_init.sh
+++ uflinux/custom_init.sh
@@ -693,15 +693,19 @@ if [ -n "$rootsubdirectory" ]; then
 	mkdir -m 0700 /realrootroot
 	mount -n -o move "${rootmnt}" "/realrootroot"
 
-	mount -o bind "/realrootroot/${rootsubdirectory}" /root
 	if [ "$rootsubdirectory_ro" = "y" ]; then
+		mount -o bind,ro "/realrootroot/${rootsubdirectory}" /root
 		mount -o remount,bind,ro "/root"
+	else
+		mount -o bind "/realrootroot/${rootsubdirectory}" /root
 	fi
 
 	if [ -d "/root/realrootroot" ]; then
-		mount -o bind /realrootroot "/root/realrootroot"
 		if [ "$realrootroot_ro" = "y" ]; then
+			mount -o bind,ro /realrootroot "/root/realrootroot"
 			mount -o remount,bind,ro "/root/realrootroot"
+		else
+			mount -o bind /realrootroot "/root/realrootroot"
 		fi
 	fi
 
