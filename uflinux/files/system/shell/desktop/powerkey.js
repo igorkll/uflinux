be3a95dba9e27b00deaed411556167539610c8d3
igorkll <frerther@gmail.com>
2026-02-12 02:27:38 +0300

    Update powerkey.js

--- uflinux/files/system/shell/desktop/powerkey.js
+++ uflinux/files/system/shell/desktop/powerkey.js
@@ -34,3 +34,32 @@ devices.forEach(path => {
     }
 })
 }
+
+{
+
+const fd = fs.openSync('/dev/input/event1', fs.constants.O_RDONLY);
+const EVENT_SIZE = 24;
+const buf = Buffer.alloc(EVENT_SIZE);
+
+function checkPowerButton() {
+    try {
+        const bytesRead = fs.readSync(fd, buf, 0, buf.length, null);
+        if (bytesRead === buf.length) {
+            const type  = buf.readUInt16LE(8);   // смещение type
+            const code  = buf.readUInt16LE(10);  // смещение code
+            const value = buf.readInt32LE(12);   // смещение value
+
+            if (type === 1) { // EV_KEY
+                console.log(`Key event: code=${code}, value=${value}`);
+            }
+        }
+    } catch(e) {
+        console.error('Read error', e);
+    }
+}
+
+console.log("QWE")
+
+setInterval(checkPowerButton, 100)
+
+}
