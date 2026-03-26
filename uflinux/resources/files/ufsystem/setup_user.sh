01cd6434120a8f166cbb79a05f515097e2c7d08a
igorkll <frerther@gmail.com>
2026-03-26 20:49:20 +0300

    backup

---
+++ uflinux/resources/files/ufsystem/setup_user.sh
@@ -0,0 +1,3 @@
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak override --user --unshare=network --disallow=all
--- uflinux/resources/runshell.sh
+++ uflinux/resources/runshell.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-/ufsystem/setup_user.sh
+if [ ! -f /data/setup_user.flag ]; then
+    /ufsystem/setup_user.sh
+    touch /data/setup_user.flag
+fi
+
 electron /ufsystem/shell --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --no-sandbox --user-data-dir=/data/electron
