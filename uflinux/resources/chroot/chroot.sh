18bbbda068a3116775dbf9bcbf9b10e885b4fa3d
igorkll <frerther@gmail.com>
2026-03-26 21:02:11 +0300

    backup

--- uflinux/resources/chroot/chroot.sh
+++ uflinux/resources/chroot/chroot.sh
@@ -7,9 +7,35 @@ npm install -g electron@39.2.7
 npm install -g ini@6.0.0
 chmod 4755 /usr/local/lib/node_modules/electron/dist/chrome-sandbox
 
+# -------------------------------- install waydroid
+
+apt install ca-certificates -y
+curl -s https://repo.waydro.id | bash
+apt update
+apt install waydroid -y
+waydroid init
+waydroid init -s GAPPS
+
+# -------------------------------- install wine
+
+dpkg --add-architecture i386
+apt update
+apt install wine
+
 # --------------------------------
 
 /ufsystem/setup_user.sh
 
+# -------------------------------- install liamounts
+
+wget https://github.com/igorkll/liamounts/archive/refs/tags/2.0.tar.gz
+tar -xzf 2.0.tar.gz
+cd liamounts-2.0
+chmod +x install.sh
+./install.sh
+cd ..
+rm -rf liamounts-2.0
+
+# -------------------------------- cleanup
 
 touch /.chrootend
--- uflinux/resources/files/ufsystem/setup_user.sh
+++ uflinux/resources/files/ufsystem/setup_user.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$EUID" -eq 0 ]; then
+    SCOPE="--system"
+else
+    SCOPE="--user"
+fi
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak override --user --unshare=network --disallow=all
+flatpak override "$SCOPE" --unshare=network --disallow=all
