5c2d43f3093b11d7f6a17d86a0d82eae9e646103
igorkll <frerther@gmail.com>
2026-03-26 20:41:10 +0300

    qwe

--- uflinux/resources/chroot/chroot.sh
+++ uflinux/resources/chroot/chroot.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+# -------------------------------- node packages
+
 npm install -g electron@39.2.7
 npm install -g ini@6.0.0
-
 chmod 4755 /usr/local/lib/node_modules/electron/dist/chrome-sandbox
 
+# -------------------------------- flatpak packages
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# --------------------------------
+
+/ufsystem/setup_user.sh
 touch /.chrootend
--- uflinux/resources/runshell.sh
+++ uflinux/resources/runshell.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+/ufsystem/setup_user.sh
 electron /ufsystem/shell --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --no-sandbox --user-data-dir=/data/electron
