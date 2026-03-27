b9a89c73255492a80a0c72af14cc31f6bb413715
igorkll <frerther@gmail.com>
2026-03-27 20:23:35 +0300

    backup

---
+++ uflinux/resources/files/ufsystem/shell/desktop/render.js
@@ -0,0 +1,28 @@
+{
+
+window.addEventListener("keydown", e => {
+    if (e.key === "Tab") {
+        e.preventDefault();
+    }
+});
+
+let timelabel = document.getElementById("timelabel")
+
+window.updateTime = function () {
+    const now = new Date()
+
+    const time = now.toLocaleTimeString('en-US', { hour12: false })     
+    const date = now.toLocaleDateString('en-US', {
+        weekday: 'long',    // "Wednesday"
+        year: 'numeric',    // "2026"
+        month: 'long',      // "February"
+        day: '2-digit'      // "11"
+    })
+
+    timelabel.innerText = time
+}
+
+updateTime()
+setInterval(updateTime, 1000)
+
+}
--- uflinux/resources/files/ufsystem/shell/render.js
+++ uflinux/resources/files/ufsystem/shell/render.js
@@ -6,22 +6,6 @@ window.addEventListener("keydown", e => {
     }
 });
 
-let timelabel = document.getElementById("timelabel")
-
-window.updateTime = function () {
-    const now = new Date()
-
-    const time = now.toLocaleTimeString('en-US', { hour12: false })     
-    const date = now.toLocaleDateString('en-US', {
-        weekday: 'long',    // "Wednesday"
-        year: 'numeric',    // "2026"
-        month: 'long',      // "February"
-        day: '2-digit'      // "11"
-    })
-
-    timelabel.innerText = time
-}
-
 //contain
 //cover
 //fill
@@ -63,7 +47,5 @@ window.updateWallpaper = function () {
 storage_load()
 
 updateWallpaper()
-updateTime()
-setInterval(updateTime, 1000)
 
 }
