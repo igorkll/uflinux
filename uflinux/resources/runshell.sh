1591292f71ed695ba51c58b225e8dc5480a1678e
igorkll <frerther@gmail.com>
2026-03-27 22:05:58 +0300

    Update runshell.sh

--- uflinux/resources/runshell.sh
+++ uflinux/resources/runshell.sh
@@ -5,5 +5,5 @@ if [ ! -f /data/setup_user.flag ]; then
     touch /data/setup_user.flag
 fi
 
-electron /ufsystem/shell lockscreen --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --no-sandbox --user-data-dir=/data/electron
-electron /ufsystem/shell shell --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --no-sandbox --user-data-dir=/data/electron
+electron /ufsystem/shell lockscreen --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --user-data-dir=/data/electron
+electron /ufsystem/shell shell --enable-gpu-rasterization --ignore-gpu-blocklist --ozone-platform=wayland --enable-features=UseOzonePlatform --user-data-dir=/data/electron
