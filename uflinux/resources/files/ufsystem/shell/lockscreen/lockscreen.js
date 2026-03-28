6c3f38a0445f3030efd48bb8e8fe41264990e329
igorkll <frerther@gmail.com>
2026-03-28 13:26:22 +0300

    backup

--- uflinux/resources/files/ufsystem/shell/lockscreen/lockscreen.js
+++ uflinux/resources/files/ufsystem/shell/lockscreen/lockscreen.js
@@ -1,5 +1,18 @@
 {
 
-const 
+const curtain = document.getElementById("curtain")
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        curtain.classList.remove("curtain-active")
+        event.preventDefault();
+    }
+    
+    if (event.key === ' ') {
+        curtain.classList.add("curtain-active")
+        event.preventDefault();
+    }
+});
 
 }
