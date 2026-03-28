5132862ae8720e6942ad9ed62fdcd350fda44dba
igorkll <frerther@gmail.com>
2026-03-28 14:04:27 +0300

    backup

--- uflinux/resources/files/ufsystem/shell/lockscreen/lockscreen.js
+++ uflinux/resources/files/ufsystem/shell/lockscreen/lockscreen.js
@@ -2,33 +2,31 @@
 
 const curtain = document.getElementById("curtain")
 
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
-        curtain.classList.remove("curtain-active")
-        event.preventDefault()
-    }
-    
-    if (event.key === " ") {
-        curtain.classList.add("curtain-active")
-        event.preventDefault()
-    }
-})
-
+let pointerdown = false
 let startY = null
 let deltaY = null
 
 curtain.addEventListener("pointerdown", (e) => {
+    pointerdown = true
     startY = e.clientY
     curtain.setPointerCapture(e.pointerId)
+    curtain.classList.add("curtain-disable-transition")
 })
 
 curtain.addEventListener("pointermove", (e) => {
     if (startY == null) return
     deltaY = e.clientY - startY
-    curtain.style.transform = "translateY(" + deltaY + "px)"
+    if (deltaY > 0) deltaY = 0
+    requestAnimationFrame(() => {
+        curtain.style.transform = "translateY(" + deltaY + "px)"
+    })
 })
 
 document.addEventListener("pointerup", () => {
+    pointerdown = false
+
+    curtain.classList.remove("curtain-disable-transition")
+
     if (deltaY < -(window.innerHeight / 4)) {
         curtain.classList.add("curtain-active")
     } else {
@@ -39,6 +37,20 @@ document.addEventListener("pointerup", () => {
 
     startY = null
     deltaY = null
+})
+
+document.addEventListener("keydown", function(event) {
+    if (pointerdown) return
+
+    if (event.key === "Escape") {
+        curtain.classList.remove("curtain-active")
+        event.preventDefault()
+    }
+    
+    if (event.key === " ") {
+        curtain.classList.add("curtain-active")
+        event.preventDefault()
+    }
 })
 
 }
