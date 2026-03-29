3da029a0b76125a51cb1da7c4a85185f96201253
igorkll <frerther@gmail.com>
2026-03-29 19:43:02 +0300

    we

--- uflinux/resources/files/ufsystem/shell/render.js
+++ uflinux/resources/files/ufsystem/shell/render.js
@@ -48,3 +48,57 @@ storage_load()
 updateWallpaper()
 
 }
+
+(function() {
+    let lastX = null, lastY = null, lastTime = null
+    const MIN_SPEED = 512
+
+    function handlePointerMove(event) {
+        let x, y
+        if (event.touches) {
+            x = event.touches[0].clientX
+            y = event.touches[0].clientY
+        } else {
+            x = event.clientX
+            y = event.clientY
+        }
+
+        const now = performance.now()
+
+        if (lastX !== null && lastY !== null && lastTime !== null) {
+            const dx = x - lastX
+            const dy = y - lastY
+            const dt = (now - lastTime) / 1000
+            const speed = Math.sqrt(dx*dx + dy*dy) / dt
+
+            if (speed >= MIN_SPEED) {
+                document.dispatchEvent(new CustomEvent('user_interaction'))
+            }
+        }
+
+        lastX = x
+        lastY = y
+        lastTime = now
+    }
+
+    function handleOther(event) {
+        document.dispatchEvent(new CustomEvent('user_interaction'))
+    }
+
+    document.addEventListener('mousemove', handlePointerMove)
+    document.addEventListener('touchmove', handlePointerMove, { passive: true })
+    document.addEventListener('touchstart', handleOther)
+    document.addEventListener('keydown', handleOther)
+    document.addEventListener('wheel', handleOther)
+})();
+
+(function() {
+    function handle(event) {
+        if (event.buttons > 0) {
+            document.dispatchEvent(new CustomEvent('active_interaction'))
+        }
+    }
+
+    document.addEventListener('pointermove', handle)
+    document.addEventListener('pointerdown', handle)
+})();
